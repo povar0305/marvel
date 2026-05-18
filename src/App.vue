048f43e26387e5375c8e3ceb41de6b7e0def988e
sonya <soficko.sofi@yandex.ru>
2026-05-18 21:49:51 +0300

    element ui проверила, все окей

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import {ref} from 'vue'
+
+  const value1 = ref(true)
+</script>
 
 <template>
   <router-view />
+
+  <el-switch v-model="value1" />
 </template>
