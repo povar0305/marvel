c8e5001cb6899cc952a4a65d5c33644953b66a08
sonya <soficko.sofi@yandex.ru>
2026-05-21 13:02:49 +0300

    автоимпорт компонентов

--- src/components.d.ts
+++ src/components.d.ts
@@ -17,8 +17,6 @@ declare module 'vue' {
     ElInput: typeof import('element-plus/es')['ElInput']
     ElPageHeader: typeof import('element-plus/es')['ElPageHeader']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    Header: typeof import('./components/header/index.vue')['default']
-    Home: typeof import('./components/home/index.vue')['default']
     MBottomMenu: typeof import('./components/m-bottom-menu/index.vue')['default']
     MHeader: typeof import('./components/m-header/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
