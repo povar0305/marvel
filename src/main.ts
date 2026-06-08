2dceec5d4e9f83e3aa5f3cc7f5efd4205dd8be24
sonya <soficko.sofi@yandex.ru>
2026-06-08 16:14:54 +0300

    Добавила tippy для подсказок

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import 'normalize.css/normalize.css'
 import 'element-plus/dist/index.css'
 import '@/styles/typography.css'
 
+import VueTippy from 'vue-tippy'
+import 'tippy.js/dist/tippy.css' // базовые стили
+import 'tippy.js/themes/light.css' // светлая тема (опционально)
 import App from './App.vue'
 import router from './router'
 
@@ -15,5 +18,14 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.use(VueTippy, {
+  directive: 'tippy',
+  component: 'tippy',
+  defaultProps: {
+    allowHTML: false,
+    delay: [300, 0],
+    animation: 'fade',
+  },
+})
 
 app.mount('#app')
