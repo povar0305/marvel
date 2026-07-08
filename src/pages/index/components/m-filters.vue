c66c69a2716d4d73c548fe5aa5957a339769bfe9
sonya <soficko.sofi@yandex.ru>
2026-07-08 19:25:52 +0300

    Перевела фильтры на русский

--- src/pages/index/components/m-filters.vue
+++ src/pages/index/components/m-filters.vue
@@ -1,88 +1,88 @@
 <script lang="ts" setup>
-  import { useHeroesStore } from '@/stores/heroes.ts'
-  import { storeToRefs } from 'pinia'
-  import { computed } from 'vue'
-  import type {
-    sort_by as sort_by_type,
-    sort_order as sort_order_type,
-    statuses as statuses_type,
-  } from '@/types/filters.ts'
+import { useHeroesStore } from '@/stores/heroes.ts'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+import type {
+  sort_by as sort_by_type,
+  sort_order as sort_order_type,
+  statuses as statuses_type,
+} from '@/types/filters.ts'
 
-  const heroesStore = useHeroesStore()
-  const {
-    universes,
-    teams,
-    statuses,
-    selected_affiliations,
-    selected_universe,
-    selected_team,
-    selected_status,
-    sort_by,
-    sort_order,
-    affiliations,
-  } = storeToRefs(heroesStore)
+const heroesStore = useHeroesStore()
+const {
+  universes,
+  teams,
+  statuses,
+  selected_affiliations,
+  selected_universe,
+  selected_team,
+  selected_status,
+  sort_by,
+  sort_order,
+  affiliations,
+} = storeToRefs(heroesStore)
 
-  /**
-   * Объект с данными по фильтрам
-   */
-  const filters = computed(() => {
-    return [
-      {
-        options: universes.value,
-        placeholder: 'Universes',
-        modelValue: selected_universe.value,
-        onUpdate: (newValue: string) => {
-          heroesStore.setUniverse(newValue as statuses_type | 'all')
-        },
+/**
+ * Объект с данными по фильтрам
+ */
+const filters = computed(() => {
+  return [
+    {
+      options: universes.value,
+      placeholder: 'Вселенная',
+      modelValue: selected_universe.value,
+      onUpdate: (newValue: string) => {
+        heroesStore.setUniverse(newValue as statuses_type | 'Все')
       },
-      {
-        options: teams.value,
-        placeholder: 'Teams',
-        modelValue: selected_team.value,
-        onUpdate: (newValue: string) => {
-          heroesStore.setTeam(newValue)
-        },
+    },
+    {
+      options: teams.value,
+      placeholder: 'Команда',
+      modelValue: selected_team.value,
+      onUpdate: (newValue: string) => {
+        heroesStore.setUniverse(newValue as string | 'Все')
       },
-      {
-        options: affiliations.value,
-        placeholder: 'Organizations',
-        modelValue: selected_affiliations.value,
-        onUpdate: (newValue: string) => {
-          heroesStore.setAffiliations(newValue)
-        },
+    },
+    {
+      options: affiliations.value,
+      placeholder: 'Организация',
+      modelValue: selected_affiliations.value,
+      onUpdate: (newValue: string) => {
+        heroesStore.setAffiliations(newValue)
       },
-      {
-        options: statuses.value,
-        placeholder: 'Status',
-        modelValue: selected_status.value,
-        onUpdate: (newValue: string) => {
-          heroesStore.setStatus(newValue as statuses_type | 'all')
-        },
+    },
+    {
+      options: statuses.value,
+      placeholder: 'Статус',
+      modelValue: selected_status.value,
+      onUpdate: (newValue: string) => {
+        heroesStore.setStatus(newValue as string | 'Все')
       },
-    ]
-  })
+    },
+  ]
+})
 
-  const sorting = computed(() => {
-    return [
-      {
-        options: heroesStore.sort_by_list,
-        modelValue: sort_by.value,
-        filterable: true,
-        onUpdate: (newValue: string) => {
-          heroesStore.setSortBy(newValue as sort_by_type)
-        },
+const sorting = computed(() => {
+  return [
+    {
+      options: heroesStore.sort_by_list,
+      modelValue: sort_by.value,
+      filterable: true,
+      onUpdate: (newValue: string) => {
+        heroesStore.setSortBy(newValue as sort_by_type)
       },
-      {
-        options: ['asc', 'desc'],
-        modelValue: sort_order.value,
-        filterable: false,
-        classes: 'max-w-20! min-w-20!',
-        onUpdate: (newValue: string) => {
-          heroesStore.setSortOrder(newValue as sort_order_type)
-        },
+    },
+    {
+      options: ['asc', 'desc'],
+      modelValue: sort_order.value,
+      filterable: false,
+      classes: 'max-w-20! min-w-20!',
+      onUpdate: (newValue: string) => {
+        heroesStore.setSortOrder(newValue as sort_order_type)
       },
-    ]
-  })
+    },
+  ]
+})
 </script>
 
 <template>
@@ -103,12 +103,7 @@
           filterable
           @update:model-value="filter.onUpdate"
         >
-          <el-option
-            v-for="item in filter.options"
-            :key="item"
-            :label="item"
-            :value="item"
-          />
+          <el-option v-for="item in filter.options" :key="item" :label="item" :value="item" />
         </el-select>
       </div>
     </div>
@@ -127,9 +122,9 @@
       >
         <el-option
           v-for="item in sort.options"
-          :key="item.key"
-          :label="item.option || item"
-          :value="item.key || item"
+          :key="item?.key"
+          :label="item?.option || item"
+          :value="item?.key || item"
         />
       </el-select>
     </div>
